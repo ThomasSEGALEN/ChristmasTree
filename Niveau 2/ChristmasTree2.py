948bdfddc5b825875bc0bc64984be0809721b1cb
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2020-12-17 11:00:18 +0100

    add content

--- Niveau 2/ChristmasTree2.py
+++ Niveau 2/ChristmasTree2.py
@@ -4,7 +4,7 @@ n = 4
 # Method1 - Trunk
 # stars_nb = 5
 # spaces_nb = height - 1
-# for i in range(n-1):
+# for i in range(n - 1):
 #     for j in range(0, 2):
 #         print(spaces_nb * "   " + stars_nb * "*")
 
